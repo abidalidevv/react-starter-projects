cfce05ff7bbc12373209a6189cbd1b95dc6d7bab
abidmmp <abidmmp99@gmail.com>
2020-06-06 10:06:00 +0500

    Fix prop drilling issue

--- core_logic.jsx
+++ core_logic.jsx
@@ -61,3 +61,4 @@
 // Fix prop drilling issue - 2023-02-19 10:04:00
 // Improve event handlers - 2022-06-16 10:00:00
 // Fix useRef typing - 2018-09-08 10:10:00
+// Fix prop drilling issue - 2020-06-06 10:06:00
