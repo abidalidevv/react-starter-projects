52368b46d772d3b56d2879f8004865e65b5dfe96
abidmmp <abidmmp99@gmail.com>
2020-08-07 10:12:00 +0500

    Fix context value

--- core_logic.jsx
+++ core_logic.jsx
@@ -91,3 +91,4 @@
 // Fix ref usage - 2021-10-06 10:10:00
 // Add aria attributes - 2019-08-04 10:06:00
 // Add action type narrowing - 2021-07-10 10:02:00
+// Fix context value - 2020-08-07 10:12:00
