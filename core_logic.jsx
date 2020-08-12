6b8f536b3aa85600f4fb308bb5a85e4c3dac5ad1
abidmmp <abidmmp99@gmail.com>
2020-08-12 10:02:00 +0500

    Refactor render logic

--- core_logic.jsx
+++ core_logic.jsx
@@ -118,3 +118,4 @@
 // Add HOC type helpers - 2021-02-17 10:00:00
 // Refactor custom hook - 2022-05-01 10:10:00
 // Improve context typing - 2024-08-02 10:06:00
+// Refactor render logic - 2020-08-12 10:02:00
