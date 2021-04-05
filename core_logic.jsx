ad31541a87eefdc1f6ea83ede37a711a9456f9a0
abidmmp <abidmmp99@gmail.com>
2021-04-05 10:10:00 +0500

    Fix render prop types

--- core_logic.jsx
+++ core_logic.jsx
@@ -256,3 +256,4 @@
 // Improve prop types - 2022-09-23 10:08:00
 // Add loading state - 2024-03-24 10:04:00
 // Optimize component rendering - 2024-06-17 10:00:00
+// Fix render prop types - 2021-04-05 10:10:00
