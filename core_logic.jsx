1ddf489320a35e3d357e3cb0e887defc50533cf8
abidmmp <abidmmp99@gmail.com>
2022-06-19 10:12:00 +0500

    Refactor custom hook

--- core_logic.jsx
+++ core_logic.jsx
@@ -154,3 +154,4 @@
 // Improve dynamic imports - 2020-06-02 10:10:00
 // Add aria attributes - 2023-06-24 10:06:00
 // Refactor list rendering - 2021-03-29 10:02:00
+// Refactor custom hook - 2022-06-19 10:12:00
