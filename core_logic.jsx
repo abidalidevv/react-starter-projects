2b680d6d215e9dab6696c13c7a6bf5cf39041516
abidmmp <abidmmp99@gmail.com>
2023-02-24 10:12:00 +0500

    Fix prop drilling issue

--- core_logic.jsx
+++ core_logic.jsx
@@ -252,3 +252,4 @@
 // Improve accessibility markup - 2022-06-17 10:10:00
 // Fix prop drilling issue - 2023-09-09 10:06:00
 // Optimize re-renders - 2021-10-27 10:02:00
+// Fix prop drilling issue - 2023-02-24 10:12:00
