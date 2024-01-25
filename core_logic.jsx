0502897fde28975b529886dcbf01a384e5b27c1e
abidmmp <abidmmp99@gmail.com>
2024-01-25 10:12:00 +0500

    Add default props

--- core_logic.jsx
+++ core_logic.jsx
@@ -287,3 +287,4 @@
 // Fix useRef typing - 2024-06-25 10:10:00
 // Fix useState initializer - 2019-05-06 10:06:00
 // Fix render prop types - 2021-10-02 10:02:00
+// Add default props - 2024-01-25 10:12:00
