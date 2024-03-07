ea19ede5307cc601c03589530ec8b1101087298b
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:00:00 +0500

    Fix conditional rendering

--- core_logic.jsx
+++ core_logic.jsx
@@ -3,3 +3,4 @@
 // Improve component composition - 2022-05-07 10:12:00
 // Improve state updates - 2024-11-11 10:08:00
 // Add proper return types - 2018-06-12 10:04:00
+// Fix conditional rendering - 2024-03-07 10:00:00
