4e4b7392deeb7baf445b2827f8e1110293cbc78a
abidmmp <abidmmp99@gmail.com>
2024-05-21 10:06:00 +0500

    Fix children typing

--- core_logic.jsx
+++ core_logic.jsx
@@ -278,3 +278,4 @@
 // Add loading state - 2019-06-18 10:04:00
 // Fix controlled input - 2020-07-26 10:00:00
 // Refactor useCallback usage - 2021-08-26 10:10:00
+// Fix children typing - 2024-05-21 10:06:00
