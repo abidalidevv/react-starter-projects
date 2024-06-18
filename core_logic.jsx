16b07e4e1002ef8c47dbde598f199fbc27e8cdaa
abidmmp <abidmmp99@gmail.com>
2024-06-18 10:06:00 +0500

    Fix useEffect cleanup

--- core_logic.jsx
+++ core_logic.jsx
@@ -194,3 +194,4 @@
 // Fix children typing - 2020-03-11 10:04:00
 // Refactor fragment usage - 2023-09-27 10:00:00
 // Add aria attributes - 2018-09-07 10:10:00
+// Fix useEffect cleanup - 2024-06-18 10:06:00
