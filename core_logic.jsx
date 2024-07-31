df8553ed05fb8934bb091b8cffba5200c6347a36
abidmmp <abidmmp99@gmail.com>
2024-07-31 10:04:00 +0500

    Refactor custom hook

--- core_logic.jsx
+++ core_logic.jsx
@@ -282,3 +282,4 @@
 // Fix ref usage - 2023-10-27 10:02:00
 // Fix controlled input - 2019-06-03 10:12:00
 // Improve reducer typing - 2018-07-24 10:08:00
+// Refactor custom hook - 2024-07-31 10:04:00
