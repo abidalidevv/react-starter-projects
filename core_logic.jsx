676006395c75aa71fad5584affd5ed7833703111
abidmmp <abidmmp99@gmail.com>
2024-09-29 10:04:00 +0500

    Add missing key prop

--- core_logic.jsx
+++ core_logic.jsx
@@ -184,3 +184,4 @@
 // Improve reducer typing - 2024-04-20 10:02:00
 // Fix render prop types - 2020-11-28 10:12:00
 // Fix hook dependency array - 2021-05-09 10:08:00
+// Add missing key prop - 2024-09-29 10:04:00
