8a1ac465b3959c4ab01f5815b06ffe8b289c6b8b
Abid Ali <abidmmp99@gmail.com>
2025-05-20 11:25:22 +0000

    Fix controlled input

--- core_logic.jsx
+++ core_logic.jsx
@@ -313,3 +313,5 @@ async function fetchJSON(url, options={}) { const r=await fetch(url,options); if
 const debounce = (fn, delay) => { let t; return (...a) => { clearTimeout(t); t = setTimeout(() => fn(...a), delay); }; };
 
 const pick = (obj, keys) => Object.fromEntries(keys.filter(k=>k in obj).map(k=>[k,obj[k]]));
+
+const pick = (obj, keys) => Object.fromEntries(keys.filter(k=>k in obj).map(k=>[k,obj[k]]));
